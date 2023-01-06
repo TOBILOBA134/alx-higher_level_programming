557323b54caa7339c0456062090ab00d5af706e9
TOBILOBA134 <orebelatobiloba@gmail.com>
2023-01-05 16:43:33 -0800

    Import a simple function from a simple file

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
